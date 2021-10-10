f9d5315967394271519790a57302964450b35ff8
Abdul Moeed <ababar.bscs15seecs@seecs.edu.pk>
2021-10-10 18:10:08 +0500

    init

---
+++ main.dart
@@ -0,0 +1,44 @@
+import 'dart:typed_data';
+import 'dart:core';
+import 'dart:math';
+
+// toBigUnsignedInt64(int number) => BigInt.from(number).toUnsigned(64);
+
+// Uint8List bigIntToUint8List(BigInt bigInt) =>
+//     bigIntToByteData(bigInt).buffer.asUint8List();
+
+List<int> martial(int number) {
+  const int radix = 8;
+  BigInt bigInt = BigInt.from(number);
+  final data = ByteData((bigInt.bitLength / radix).ceil());
+  var _bigInt = bigInt;
+  for (var i = 1; i <= data.lengthInBytes; i++) {
+    data.setUint8(data.lengthInBytes - i, _bigInt.toUnsigned(radix).toInt());
+    _bigInt = _bigInt >> radix;
+  }
+
+  return data.buffer.asUint8List().toList();
+}
+
+int unmartial(List<int> bytes) {
+  // ByteData data = Uint8List.fromList(array).buffer.asByteData();
+  BigInt result = BigInt.from(0);
+  for (int i = 0; i < bytes.length; i++) {
+    result += BigInt.from(bytes[bytes.length - i - 1]) << (8 * i);
+  }
+  return result.toUnsigned(64).toInt();
+}
+
+main() {
+  print("Start");
+  var res1 = martial(5);
+  print(res1);
+  print(unmartial(res1));
+
+  res1 = martial(1633767686115);
+  print(res1);
+  print(unmartial(res1));
+  // 1633767686115
+  // 00000001 01111100 01100100 00100100 01111111 11100011
+  // [1, 124, 100, 36, 127, 227]
+}
